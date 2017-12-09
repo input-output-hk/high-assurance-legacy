719a6fc1c4d95944b6e69da1ccadc732df8d31fd
Lars Bruenjes <brunjlar@gmail.com>
2017-12-09 06:36:44 +0100

    documented MonadDeltaQ

--- Psi/src/MonadDeltaQ.hs
+++ Psi/src/MonadDeltaQ.hs
@@ -42,12 +42,21 @@ class MonadPlus m => MonadDeltaQ m where
     -- and a handle on the rest of the other computation.
     sync    :: m a -> m b -> m (Either (a, m b) (b, m a))
 
+-- | /First-to-finish synchronization/: Runs to computation in parallel and
+-- returns the first result, discarding the second computation.
+--
+-- __Note__: The 'Alternative'-instance should be defined with  @('<|>') = 'ftf'@.
 ftf :: MonadDeltaQ m => m a -> m a -> m a
 ftf x y = either fst fst <$> sync x y
 
+-- | Executes the specified computation with the specified timeout.
+-- Returns @'Just'@ the result if the computation finishes in time
+-- and @'Nothing'@ otherwise.
 giveUpAfter :: MonadDeltaQ m => Natural -> m a -> m (Maybe a)
 giveUpAfter s ma = ftf (Just <$> ma) (const Nothing <$> vitiate (dirac s))
 
+-- | Retries the specified computation forever, using the specified timeout for
+-- each try. The result is the result of the first try that succeeds.
 retryForever :: forall a m. MonadDeltaQ m => m a -> Natural -> m a
 retryForever ma timeout = f' empty
   where
@@ -58,8 +67,13 @@ retryForever ma timeout = f' empty
             Left (a, _)      -> return a
             Right ((), ma'') -> f' ma''
 
--- retryForever s m = retryMany m $ repeat (dirac s)
-
+-- | Tries the specified computation once for each element in the specified list
+-- of timeouts. This list can be finite or infinite. Result is the result of the
+-- first try that succeeds. The computation fails if no try succeeds.
+--
+-- @'retryForever'@ is a special case of @'retryMany'@:
+--
+-- > retryForever m = retryMany m . repeat . dirac
 retryMany :: forall a m. MonadDeltaQ m => m a -> [DTime] -> m a
 retryMany ma timeouts = f' timeouts empty
   where
@@ -71,6 +85,8 @@ retryMany ma timeouts = f' timeouts empty
             Left (a, _)      -> return a
             Right ((), ma'') -> f' ts ma''
 
+-- | /Last-to-finish synchronization/: Runs two computations in parallel,
+-- returning the results of both computations as a pair of results.
 ltf :: MonadDeltaQ m => m a -> m b -> m (a, b)
 ltf x y = do
     e <- sync x y
@@ -78,5 +94,8 @@ ltf x y = do
         Left (a, mb)  -> mb >>= \b -> return (a, b)
         Right (b, ma) -> ma >>= \a -> return (a, b)
 
+-- | Runs the specified computation for /at least/
+-- the specified time: If the computation finishes earlier,
+-- returing the result is delayed until the time is up.
 waitAtLeast :: MonadDeltaQ m => DTime -> m a -> m a
 waitAtLeast s = fmap snd . ltf (vitiate s)
