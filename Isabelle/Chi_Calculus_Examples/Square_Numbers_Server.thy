7b78c948e6e03c1b3421048fb891deb66498ac18
Wolfgang Jeltsch <wolfgang@well-typed.com>
2018-07-20 16:57:33 +0300

    Add an example app to `Square_Numbers_Server`

--- Isabelle/Chi_Calculus_Examples/Square_Numbers_Server.thy
+++ Isabelle/Chi_Calculus_Examples/Square_Numbers_Server.thy
@@ -7,7 +7,7 @@ text \<open>
 \<close>
 
 theory Square_Numbers_Server
-  imports Chi_Calculus.Processes
+  imports Chi_Calculus.Processes "HOL-Library.Discrete"
 begin
 
 text \<open>
@@ -26,6 +26,8 @@ text \<open>
 
 datatype 'chan name =
     ServeInvoke nat 'chan 'chan
+  | SubmitNumbersInvoke "(nat list)" 'chan
+  | ReceiveAnnouncementsInvoke 'chan
 
 text \<open>
   Now, we implement the server process.
@@ -48,5 +50,43 @@ where
 definition square_num_server :: "'chan \<Rightarrow> 'chan \<Rightarrow> ('chan name, 'chan, 'chan val) process"
 where
   "square_num_server submissions announcements \<equiv> serve (0, submissions, announcements)"
+
+text \<open>
+  The following is a possible implementation of a client process which sends a list of square
+  numbers to the server process and receives announcements from it.
+\<close>
+
+fun submit_numbers :: "nat list \<Rightarrow> 'chan \<Rightarrow> ('chan name, 'chan, 'chan val) process"
+where
+  "submit_numbers numbers submissions = (
+    case numbers of
+      []       \<Rightarrow> \<zero> |
+      (n # ns) \<Rightarrow>
+        \<cdot>submissions \<triangleleft> SquareNumSubmission n (Discrete.sqrt n)
+        \<parallel>
+        \<langle>SubmitNumbersInvoke ns submissions\<rangle> Nothing)"
+
+definition receive_announcements :: "'chan \<Rightarrow> ('chan name, 'chan, 'chan val) process"
+where
+  "receive_announcements announcements \<equiv>
+    \<cdot>announcements \<triangleright> a. \<langle>ReceiveAnnouncementsInvoke announcements\<rangle> Nothing"
+
+definition square_num_client :: "nat list \<Rightarrow> 'chan \<Rightarrow> 'chan \<Rightarrow> ('chan name, 'chan, 'chan val) process"
+where
+  "square_num_client numbers submissions announcements \<equiv>
+    submit_numbers numbers submissions \<parallel> receive_announcements announcements"
+
+text \<open>
+  Finally, we define an application comprised by a server and two clients.
+\<close>
+
+definition square_num_app :: "('chan name, 'chan, 'chan val) process"
+where
+  "square_num_app \<equiv> \<nu> submissions announcements. (
+    square_num_server submissions announcements
+    \<parallel>
+    square_num_client [16,4,25] submissions announcements
+    \<parallel>
+    square_num_client [4,81] submissions announcements)"
 
 end
