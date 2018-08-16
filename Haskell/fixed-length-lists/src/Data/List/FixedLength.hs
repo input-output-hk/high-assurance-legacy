4a6643b821d7885006fd81690e0872a006d1ba57
Wolfgang Jeltsch <wolfgang@well-typed.com>
2018-08-17 01:12:01 +0300

    Add `fixed-length-lists` package

---
+++ Haskell/fixed-length-lists/src/Data/List/FixedLength.hs
@@ -0,0 +1,43 @@
+module Data.List.FixedLength (
+
+    List (Empty, (:::)),
+    map,
+    zipWith,
+    iterate,
+    firstNaturals
+
+) where
+
+import Prelude hiding (map, zipWith, iterate)
+
+import Data.Type.Natural (Natural (Z, S), TypeNatural (induct))
+
+data List n a where
+
+    Empty :: List 'Z a
+
+    (:::) :: a -> List n a -> List ('S n) a
+
+deriving instance Eq a => Eq (List n a)
+
+deriving instance Ord a => Ord (List n a)
+
+deriving instance Show a => Show (List n a)
+
+map :: (a -> b) -> List n a -> List n b
+map _ Empty      = Empty
+map f (x ::: xs) = f x ::: map f xs
+
+zipWith :: (a -> b -> c) -> List n a -> List n b -> List n c
+zipWith _ Empty      Empty      = Empty
+zipWith f (x ::: xs) (y ::: ys) = f x y ::: zipWith f xs ys
+
+newtype IterateAccum a n = IterateAccum { fromIterateAccum :: a -> List n a }
+
+iterate :: TypeNatural n => (a -> a) -> a -> List n a
+iterate f = fromIterateAccum $
+            induct (IterateAccum $ const Empty)
+                   (\ h -> IterateAccum $ \ x -> x ::: fromIterateAccum h (f x))
+
+firstNaturals :: (TypeNatural n, Enum a, Num a) => List n a
+firstNaturals = iterate succ 0
---
+++ Haskell/fixed-length-lists/src/Data/Type/Natural.hs
@@ -0,0 +1,24 @@
+module Data.Type.Natural (
+
+    Natural (Z, S),
+    TypeNatural (induct)
+
+) where
+
+data Natural where
+
+    Z :: Natural
+
+    S :: Natural -> Natural
+
+class TypeNatural n where
+
+    induct :: f 'Z -> (forall m . f m -> f ('S m)) -> f n
+
+instance TypeNatural 'Z where
+
+    induct z _ = z
+
+instance TypeNatural n => TypeNatural ('S n) where
+
+    induct z s = s (induct z s)
