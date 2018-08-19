33cff0482d3dc94aa03f1d85fa71b842cdb70e1e
Wolfgang Jeltsch <wolfgang@well-typed.com>
2018-08-19 21:42:45 +0300

    Improve execution of parallel composition
    Now execution of a parallel composition will only finish once execution
    of both subprocess has finished.

--- Haskell/chi-calculus/src/Ouroboros/Chi_Calculus/Process/Exec.hs
+++ Haskell/chi-calculus/src/Ouroboros/Chi_Calculus/Process/Exec.hs
@@ -8,6 +8,7 @@ import Prelude hiding (map)
 
 import Control.Concurrent (forkIO)
 import Control.Concurrent.MVar (MVar, putMVar, takeMVar, newEmptyMVar)
+import Control.Monad (void)
 
 import Data.Function (fix)
 import Data.Functor.Identity (Identity (Identity, runIdentity))
@@ -23,9 +24,17 @@ exec dataInter = worker
     worker Stop                 = return ()
     worker (Send chan val)      = putMVar chan (runIdentity (dataInter val))
     worker (Receive chan cont)  = takeMVar chan >>= worker . cont . Identity
-    worker (Parallel prc1 prc2) = forkIO (worker prc1) >>
-                                  forkIO (worker prc2) >>
-                                  return ()
+    worker (Parallel prc1 prc2) = do
+                                      finishedVar1 <- newEmptyMVar
+                                      finishedVar2 <- newEmptyMVar
+                                      void $ forkIO $ do
+                                          worker prc1
+                                          putMVar finishedVar1 ()
+                                      void $ forkIO $ do
+                                          worker prc2
+                                          putMVar finishedVar2 ()
+                                      takeMVar finishedVar1
+                                      takeMVar finishedVar2
     worker (NewChannel cont)    = newEmptyMVar >>= worker . cont
     worker (Var meaning)        = meaning
     worker (Letrec defs res)    = worker (res (fix (map worker . defs)))
