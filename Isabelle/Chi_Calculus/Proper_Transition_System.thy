15afd5a399e9ad61cc9c39b8bb3bbf2992a6bcfe
Wolfgang Jeltsch <wolfgang@well-typed.com>
2018-09-12 23:28:53 +0300

    Change an overlooked `unicast_input` to `receive`

--- Isabelle/Chi_Calculus/Proper_Transition_System.thy
+++ Isabelle/Chi_Calculus/Proper_Transition_System.thy
@@ -556,7 +556,7 @@ proof (standard, intro allI, intro impI)
   qed
 qed
 
-lemma proper_unicast_input_scope_extension: "c \<triangleright> x. \<nu> a. \<P> x a \<sim>\<^sub>\<sharp> \<nu> a. c \<triangleright> x. \<P> x a"
+lemma proper_receive_scope_extension: "c \<triangleright> x. \<nu> a. \<P> x a \<sim>\<^sub>\<sharp> \<nu> a. c \<triangleright> x. \<P> x a"
   by standard (
     fact proper_pre_receive_scope_extension_ltr,
     fact proper_pre_receive_scope_extension_rtl
