42604454cc8d38a990d3a080b06acb08bbf53329
Javier Díaz <javier.diaz@iohk.io>
2018-09-28 15:53:52 -0300

    Introduce the `Communication_Medium` example

---
+++ Isabelle/Chi_Calculus_Examples/Communication_Medium.thy
@@ -0,0 +1,32 @@
+theory Communication_Medium
+  imports
+    "HOL-Library.BNF_Corec"
+    Chi_Calculus.Typed_Processes
+begin
+
+corec
+  sender :: "['a::countable channel, 'a::countable channel] \<Rightarrow> process"
+where
+  "sender inp inpm = inp \<triangleright>\<degree> x. (inpm \<triangleleft>\<degree> x \<parallel> sender inp inpm)"
+
+corec
+  receiver :: "['a::countable channel, 'a::countable channel] \<Rightarrow> process"
+where
+  "receiver outm out = outm  \<triangleright>\<degree> x. (out \<triangleleft>\<degree> x \<parallel> receiver out outm)"
+
+corec
+  medium :: "['a::countable channel, 'a::countable channel] \<Rightarrow> process"
+where
+  "medium inpm outm = inpm \<triangleright>\<degree> x. (outm \<triangleleft>\<degree> x \<parallel> medium inpm outm)"
+
+abbreviation
+  impl :: "['a::countable channel, 'a::countable channel] \<Rightarrow> process"
+where
+  "impl inp out \<equiv> \<nu>\<degree> inpm outm. (sender inp inpm \<parallel> medium inpm outm \<parallel> receiver outm out)"
+
+corec
+  spec :: "['a::countable channel, 'a::countable channel] \<Rightarrow> process"
+where
+  "spec inp out = inp \<triangleright>\<degree> x. (out \<triangleleft>\<degree> x \<parallel> spec inp out)"
+
+end
