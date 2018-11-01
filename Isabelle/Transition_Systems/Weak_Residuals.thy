3d5374811eb36f0530a66b551726c321e0e900ca
Wolfgang Jeltsch <wolfgang@well-typed.com>
2018-11-02 00:18:48 +0200

    Add generic weak transition system framework

---
+++ Isabelle/Transition_Systems/Weak_Residuals.thy
@@ -0,0 +1,83 @@
+section \<open>Weak Residuals\<close>
+
+theory Weak_Residuals
+  imports Residuals
+begin
+
+locale weak_residual =
+  fixes silent :: "['process, 'residual] \<Rightarrow> bool"
+  fixes absorb :: "(['process, 'residual] \<Rightarrow> bool) \<Rightarrow> (['residual, 'residual] \<Rightarrow> bool)"
+  assumes absorb_monotonicity [mono]:
+    "\<I> \<le> \<J> \<Longrightarrow> absorb \<I> \<le> absorb \<J>"
+  assumes left_neutrality:
+    "silent OO absorb \<I> = \<I>"
+  assumes right_neutrality:
+    "absorb silent = op ="
+  assumes associativity:
+    "absorb \<I> OO absorb \<J> = absorb (\<I> OO absorb \<J>)"
+  assumes lift_conversion_preservation:
+    "absorb (\<X>\<inverse>\<inverse> OO silent) = (absorb (\<X> OO silent))\<inverse>\<inverse>"
+begin
+
+text \<open>
+  Using \<open>absorb_monotonicity\<close>, we define a proof method for reasoning under \<^term>\<open>absorb\<close>. This
+  method works analogously to \<open>under_lift\<close> and \<open>under_transfer\<close>.
+\<close>
+
+method under_absorb = (elim predicate2D [OF absorb_monotonicity, OF predicate2I, rotated])
+
+abbreviation lift :: "(['process, 'process] \<Rightarrow> bool) \<Rightarrow> (['residual, 'residual] \<Rightarrow> bool)" where
+  "lift \<X> \<equiv> absorb (\<X> OO silent)"
+
+sublocale residual lift
+proof unfold_locales
+  fix \<X> :: "['process, 'process] \<Rightarrow> bool" and \<Y>
+  assume "\<X> \<le> \<Y>"
+  then have "\<X> OO silent \<le> \<Y> OO silent"
+    by (simp add: relcompp_mono)
+  then show "absorb (\<X> OO silent) \<le> absorb (\<Y> OO silent)"
+    by (fact absorb_monotonicity)
+next
+  show "absorb (op = OO silent) = op ="
+    by (simp add: eq_OO right_neutrality)
+next
+  fix \<X> :: "['process, 'process] \<Rightarrow> bool" and \<Y>
+  have "absorb ((\<X> OO \<Y>) OO silent) = absorb (\<X> OO (\<Y> OO silent))"
+    by (simp add: relcompp_assoc)
+  also have "\<dots> = absorb (\<X> OO (silent OO absorb (\<Y> OO silent)))"
+    by (simp add: left_neutrality)
+  also have "\<dots> = absorb ((\<X> OO silent) OO absorb (\<Y> OO silent))"
+    by (simp add: relcompp_assoc)
+  also have "\<dots> = absorb (\<X> OO silent) OO absorb (\<Y> OO silent)"
+    by (simp add: associativity)
+  finally show "absorb ((\<X> OO \<Y>) OO silent) = absorb (\<X> OO silent) OO absorb (\<Y> OO silent)" .
+next
+  fix \<X>
+  show "absorb (\<X>\<inverse>\<inverse> OO silent) = (absorb (\<X> OO silent))\<inverse>\<inverse>"
+    by (fact lift_conversion_preservation)
+qed
+
+lemma silent_naturality: "\<X> OO silent = silent OO lift \<X>"
+proof -
+  show "\<X> OO silent = silent OO absorb (\<X> OO silent)"
+    by (simp add: left_neutrality)
+qed
+lemma absorb_pre_naturality: "absorb (\<X> OO \<I>) = lift \<X> OO absorb \<I>"
+proof -
+  have "absorb (\<X> OO \<I>) = absorb (\<X> OO (silent OO absorb \<I>))"
+    by (simp add: left_neutrality)
+  also have "\<dots> = absorb ((\<X> OO silent) OO absorb \<I>)"
+    by (simp add: relcompp_assoc)
+  also have "\<dots> = absorb (\<X> OO silent) OO absorb \<I>"
+    by (simp add: associativity)
+  finally show ?thesis .
+qed
+lemma absorb_post_naturality: "absorb (\<I> OO lift \<Y>) = absorb \<I> OO lift \<Y>"
+  by (simp add: associativity)
+lemma absorb_naturality: "absorb (\<X> OO \<I> OO lift \<Y>) = lift \<X> OO absorb \<I> OO lift \<Y>"
+  using absorb_pre_naturality and absorb_post_naturality
+  by metis
+
+end
+
+end
---
+++ Isabelle/Transition_Systems/Weak_Transition_Systems.thy
@@ -0,0 +1,125 @@
+section \<open>Weak Transition Systems\<close>
+
+theory Weak_Transition_Systems
+  imports Transition_Systems Weak_Residuals
+begin
+
+locale weak_transition_system =
+  weak_residual silent absorb for silent :: "['process, 'residual] \<Rightarrow> bool" and absorb +
+  fixes strong_transition :: "['process, 'residual] \<Rightarrow> bool" (infix "\<rightarrow>" 50)
+begin
+
+sublocale strong: transition_system lift strong_transition
+  by intro_locales
+
+notation strong.pre_bisimilarity (infix "\<lesssim>" 50)
+notation strong.bisimilarity (infix "\<sim>" 50)
+
+inductive weak_transition :: "['process, 'residual] \<Rightarrow> bool" (infix "\<Rightarrow>" 50) where
+  strong_transition: "p \<rightarrow> d \<Longrightarrow> p \<Rightarrow> d" |
+  silent_transition: "silent p d \<Longrightarrow> p \<Rightarrow> d" |
+  composed_transition: "\<lbrakk>p \<Rightarrow> d; absorb op \<Rightarrow> d e\<rbrakk> \<Longrightarrow> p \<Rightarrow> e"
+
+sublocale weak: transition_system lift weak_transition
+  by intro_locales
+
+notation weak.pre_bisimilarity (infix "\<lessapprox>" 50)
+notation weak.bisimilarity (infix "\<approx>" 50)
+
+sublocale mixed: simulation_system lift strong_transition weak_transition
+  by intro_locales
+
+lemma weak_simulation_equivalence: "weak.sim \<X> \<longleftrightarrow> mixed.sim \<X>"
+proof
+  assume "\<X> \<le> weak.transfer \<X>" 
+  also have "\<dots> \<le> mixed.transfer \<X>" by (blast intro: strong_transition)
+  finally show "\<X> \<le> mixed.transfer \<X>" .
+next
+  assume "\<X> \<le> mixed.transfer \<X>"
+  show "\<X> \<le> weak.transfer \<X>"
+  proof (intro le_funI, intro le_boolI, intro allI, intro impI)
+    fix p and q and d
+    assume "p \<Rightarrow> d" and "\<X> p q"
+    then show "\<exists>e. q \<Rightarrow> e \<and> lift \<X> d e"
+    proof (induction arbitrary: q)
+      case (strong_transition p d q)
+      with `\<X> \<le> mixed.transfer \<X>` show ?case by blast
+    next
+      case (silent_transition p d q)
+      then have "(\<X>\<inverse>\<inverse> OO silent) q d"
+        by blast
+      then have "(silent OO lift \<X>\<inverse>\<inverse>) q d"
+        using silent_naturality
+        by fastforce
+      then obtain e where "silent q e" and "lift \<X> d e"
+        using lift_conversion_preservation
+        by fastforce
+      then show ?case
+        by (blast intro: weak_transition.silent_transition)
+    next
+      case (composed_transition p d\<^sub>1 d' q)
+      from composed_transition.IH(1) and `\<X> p q` obtain e\<^sub>1 where "q \<Rightarrow> e\<^sub>1" and "lift \<X> d\<^sub>1 e\<^sub>1"
+        by blast
+      let ?IH_2_core = "\<lambda>p\<^sub>1 d\<^sub>2. \<forall>q\<^sub>1. \<X> p\<^sub>1 q\<^sub>1 \<longrightarrow> (\<exists>e\<^sub>2. q\<^sub>1 \<Rightarrow> e\<^sub>2 \<and> lift \<X> d\<^sub>2 e\<^sub>2)"
+      from composed_transition.IH(2) have "absorb ?IH_2_core d\<^sub>1 d'"
+        by under_absorb (elim conjunct2)
+      with `lift \<X> d\<^sub>1 e\<^sub>1` have "(lift \<X>\<inverse>\<inverse> OO absorb ?IH_2_core) e\<^sub>1 d'"
+        using lift_conversion_preservation
+        by fastforce
+      then have "absorb (\<X>\<inverse>\<inverse> OO ?IH_2_core) e\<^sub>1 d'"
+        using absorb_pre_naturality
+        by metis
+      then have "absorb (op \<Rightarrow> OO lift \<X>\<inverse>\<inverse>) e\<^sub>1 d'"
+      proof under_absorb
+        fix q\<^sub>1 and d\<^sub>2
+        assume "(\<X>\<inverse>\<inverse> OO ?IH_2_core) q\<^sub>1 d\<^sub>2"
+        then obtain p\<^sub>1 where "\<X> p\<^sub>1 q\<^sub>1" and "?IH_2_core p\<^sub>1 d\<^sub>2"
+          by blast
+        then obtain e\<^sub>2 where "q\<^sub>1 \<Rightarrow> e\<^sub>2" and "lift \<X> d\<^sub>2 e\<^sub>2"
+          by blast
+        then show "(op \<Rightarrow> OO lift \<X>\<inverse>\<inverse>) q\<^sub>1 d\<^sub>2"
+          using lift_conversion_preservation
+          by fastforce
+      qed
+      then have "(absorb op \<Rightarrow> OO lift \<X>\<inverse>\<inverse>) e\<^sub>1 d'"
+        by (simp add: absorb_post_naturality)
+      then obtain e' where "absorb op \<Rightarrow> e\<^sub>1 e'" and "lift \<X> d' e'"
+        using lift_conversion_preservation
+        by fastforce
+      with `q \<Rightarrow> e\<^sub>1` show ?case
+        by (blast intro: weak_transition.composed_transition)
+    qed
+  qed
+qed
+lemma weak_bisimulation_equivalence: "weak.bisim \<X> \<longleftrightarrow> mixed.bisim \<X>"
+  by (simp add: weak_simulation_equivalence)
+lemma weak_bisimilarity_equality: "weak.bisimilarity = mixed.bisimilarity"
+proof -
+  have "weak.bisimilarity = (GREATEST \<X>. weak.bisim \<X>)"
+    by (fact weak.bisimilarity_is_greatest_bisimulation)
+  also have "\<dots> = (GREATEST \<X>. mixed.bisim \<X>)"
+    by (simp add: weak_bisimulation_equivalence)
+  also have "\<dots> = mixed.bisimilarity"
+    by (simp add: mixed.bisimilarity_is_greatest_bisimulation)
+  finally show ?thesis .
+qed
+
+lemma strong_simulation_is_weak_simulation: "strong.sim \<X> \<Longrightarrow> weak.sim \<X>"
+proof -
+  assume "\<X> \<le> strong.transfer \<X>"
+  also have "\<dots> \<le> mixed.transfer \<X>" by (blast intro: strong_transition)
+  finally have "mixed.sim \<X>" .
+  then show "weak.sim \<X>" by (simp add: weak_simulation_equivalence)
+qed
+lemma strong_bisimulation_is_weak_bisimulation: "strong.bisim \<X> \<Longrightarrow> weak.bisim \<X>"
+  using strong_simulation_is_weak_simulation by blast
+lemma strong_bisimilarity_in_weak_bisimilarity: "op \<sim> \<le> op \<approx>"
+proof -
+  have "strong.bisim op \<sim>" by (fact strong.bisimilarity_is_bisimulation)
+  then have "weak.bisim op \<sim>" by (fact strong_bisimulation_is_weak_bisimulation)
+  then show "op \<sim> \<le> op \<approx>" by (fact weak.bisimulation_in_bisimilarity)
+qed
+
+end
+
+end
