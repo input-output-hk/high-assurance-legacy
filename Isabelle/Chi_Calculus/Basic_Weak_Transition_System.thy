28eb137a621d8bb1cbb77cc96a0892ee2bce71ba
Javier Díaz <javier.diaz@iohk.io>
2019-01-21 18:56:22 -0300

    Rename some concepts

--- Isabelle/Chi_Calculus/Basic_Weak_Transition_System.thy
+++ Isabelle/Chi_Calculus/Basic_Weak_Transition_System.thy
@@ -36,22 +36,24 @@ qed
 interpretation basic: weak_transition_system basic_silent basic.absorb basic_transition
   by intro_locales
 
-(* Sequence of silent transitions: \<Rightarrow>\<^sup>\<tau>\<^sub>\<flat> *)
+(* Basic weak transition \<Rightarrow>\<^sub>\<flat> *)
 
 notation basic.weak_transition (infix "\<Rightarrow>\<^sub>\<flat>" 50)
 
-abbreviation
-  silent_sequence :: "[process, process] \<Rightarrow> bool" (infix "\<Rightarrow>\<^sup>\<tau>\<^sub>\<flat>" 50)
-where
-  "silent_sequence p q \<equiv> p \<Rightarrow>\<^sub>\<flat>\<lbrace>\<tau>\<rbrace> q"
+(* Sequence of basic silent transitions: \<Rightarrow>\<^sup>\<tau>\<^sub>\<flat> *)
 
-lemma silent_sequence_refl: "p \<Rightarrow>\<^sup>\<tau>\<^sub>\<flat> p"
+abbreviation
+  basic_silent_sequence :: "[process, process] \<Rightarrow> bool" (infix "\<Rightarrow>\<^sup>\<tau>\<^sub>\<flat>" 50)
+where
+  "basic_silent_sequence p q \<equiv> p \<Rightarrow>\<^sub>\<flat>\<lbrace>\<tau>\<rbrace> q"
+
+lemma basic_silent_sequence_refl: "p \<Rightarrow>\<^sup>\<tau>\<^sub>\<flat> p"
   by (simp add: basic.silent_transition basic_internal_is_silent)
 
-lemma silent_sequence_trans [trans]: "\<lbrakk> p \<Rightarrow>\<^sup>\<tau>\<^sub>\<flat> r; r \<Rightarrow>\<^sup>\<tau>\<^sub>\<flat> q \<rbrakk> \<Longrightarrow> p \<Rightarrow>\<^sup>\<tau>\<^sub>\<flat> q"
+lemma basic_silent_sequence_trans [trans]: "\<lbrakk> p \<Rightarrow>\<^sup>\<tau>\<^sub>\<flat> r; r \<Rightarrow>\<^sup>\<tau>\<^sub>\<flat> q \<rbrakk> \<Longrightarrow> p \<Rightarrow>\<^sup>\<tau>\<^sub>\<flat> q"
   by (blast intro: basic.composed_transition basic_internal_is_silent)
 
-lemma silent_sequence_non_empty: "\<lbrakk> p \<Rightarrow>\<^sub>\<flat>\<lbrace>\<tau>\<rbrace> q; p \<noteq> q \<rbrakk> \<Longrightarrow> \<exists>r. p \<rightarrow>\<^sub>\<flat>\<lbrace>\<tau>\<rbrace> r \<and> r \<Rightarrow>\<^sup>\<tau>\<^sub>\<flat> q"
+lemma basic_silent_sequence_non_empty: "\<lbrakk> p \<Rightarrow>\<^sub>\<flat>\<lbrace>\<tau>\<rbrace> q; p \<noteq> q \<rbrakk> \<Longrightarrow> \<exists>r. p \<rightarrow>\<^sub>\<flat>\<lbrace>\<tau>\<rbrace> r \<and> r \<Rightarrow>\<^sup>\<tau>\<^sub>\<flat> q"
 proof -
   fix p q
   assume "p \<Rightarrow>\<^sub>\<flat>\<lbrace>\<tau>\<rbrace> q" and "p \<noteq> q"
@@ -59,7 +61,7 @@ proof -
   proof (induction "\<lbrace>\<tau>\<rbrace> q" arbitrary: q)
     case (strong_transition p)
     then have "q \<Rightarrow>\<^sup>\<tau>\<^sub>\<flat> q"
-      using silent_sequence_refl by simp
+      using basic_silent_sequence_refl by simp
     moreover have "p \<rightarrow>\<^sub>\<flat>\<lbrace>\<tau>\<rbrace> q"
       by (blast intro: strong_transition.hyps strong_transition.prems)
     ultimately show ?case
@@ -83,41 +85,43 @@ proof -
       then have "p\<^sub>1 \<Rightarrow>\<^sub>\<flat>\<lbrace>\<tau>\<rbrace> q \<and> (p\<^sub>1 \<noteq> q \<longrightarrow> (\<exists>r. p\<^sub>1 \<rightarrow>\<^sub>\<flat>\<lbrace>\<tau>\<rbrace> r \<and> r \<Rightarrow>\<^sup>\<tau>\<^sub>\<flat> q))"
         using \<open>p \<noteq> q\<close> by simp
       then show ?case
-        using \<open>c = \<lbrace>\<tau>\<rbrace> p\<^sub>1\<close> and composed_transition.hyps(2) and composed_transition.prems and silent_sequence_trans
+        using \<open>c = \<lbrace>\<tau>\<rbrace> p\<^sub>1\<close> and composed_transition.hyps(2) and composed_transition.prems and basic_silent_sequence_trans
         by blast
     next
       assume "basic.absorb_upward ?\<I> c (\<lbrace>\<tau>\<rbrace> q)"
       then obtain p\<^sub>1 where "c = \<lbrace>\<tau>\<rbrace> p\<^sub>1" and "p\<^sub>1 \<Rightarrow>\<^sub>\<flat>\<lbrace>\<tau>\<rbrace> q \<and> (p\<^sub>1 \<noteq> q \<longrightarrow> (\<exists>r. p\<^sub>1 \<rightarrow>\<^sub>\<flat>\<lbrace>\<tau>\<rbrace> r \<and> r \<Rightarrow>\<^sup>\<tau>\<^sub>\<flat> q))"
         using basic_lift.simps and basic_silent.cases by fastforce
       then show ?thesis
-        using composed_transition.hyps(2) and composed_transition.prems and silent_sequence_trans
+        using composed_transition.hyps(2) and composed_transition.prems and basic_silent_sequence_trans
         by blast
     qed
   qed
 qed
 
-(** Lifted weak silent-respecting basic operational semantics **)
+lemma basic_weak_transition_acting_elim: "p \<Rightarrow>\<^sub>\<flat>\<lbrace>\<alpha>\<rbrace> q \<Longrightarrow> \<exists>r s. p \<Rightarrow>\<^sup>\<tau>\<^sub>\<flat> r \<and> r \<rightarrow>\<^sub>\<flat>\<lbrace>\<alpha>\<rbrace> s \<and> s \<Rightarrow>\<^sup>\<tau>\<^sub>\<flat> q" sorry
 
-lemma weak_silent_sending: "a \<triangleleft> x \<Rightarrow>\<^sub>\<flat>\<lbrace>a \<triangleleft> x\<rbrace> \<zero>"
+(* Lifted basic weak operational semantics *)
+
+lemma basic_weak_transition_sending: "a \<triangleleft> x \<Rightarrow>\<^sub>\<flat>\<lbrace>a \<triangleleft> x\<rbrace> \<zero>"
   using sending by (simp add: basic.strong_transition)
 
-lemma weak_silent_receiving: "a \<triangleright> x. P x \<Rightarrow>\<^sub>\<flat>\<lbrace>a \<triangleright> x\<rbrace> P x"
+lemma basic_weak_transition_receiving: "a \<triangleright> x. P x \<Rightarrow>\<^sub>\<flat>\<lbrace>a \<triangleright> x\<rbrace> P x"
   using receiving by (simp add: basic.strong_transition)
 
-(** Weak basic transition \<Rightarrow>\<^sub>\<flat>\<^sup>^ **)
+(** Basic weak simulating transition \<Rightarrow>\<^sub>\<flat>\<^sup>^ **)
 
 inductive
-  weak_basic_transition :: "process \<Rightarrow> basic_residual \<Rightarrow> bool" (infix "\<Rightarrow>\<^sub>\<flat>\<^sup>^" 50)
+  basic_weak_simulating_transition :: "process \<Rightarrow> basic_residual \<Rightarrow> bool" (infix "\<Rightarrow>\<^sub>\<flat>\<^sup>^" 50)
 where
-  weak_basic_transition_acting: "p \<Rightarrow>\<^sub>\<flat>\<lbrace>\<alpha>\<rbrace> q \<or> (\<alpha> = \<tau> \<and> p = q) \<Longrightarrow> p \<Rightarrow>\<^sub>\<flat>\<^sup>^\<lbrace>\<alpha>\<rbrace> q" |
-  weak_basic_transition_opening: "p \<Rightarrow>\<^sub>\<flat>\<lbrace>\<nu> a\<rbrace> P a \<Longrightarrow> p \<Rightarrow>\<^sub>\<flat>\<^sup>^\<lbrace>\<nu> a\<rbrace> P a"
+  basic_weak_simulating_transition_acting: "p \<Rightarrow>\<^sub>\<flat>\<lbrace>\<alpha>\<rbrace> q \<or> (\<alpha> = \<tau> \<and> p = q) \<Longrightarrow> p \<Rightarrow>\<^sub>\<flat>\<^sup>^\<lbrace>\<alpha>\<rbrace> q" |
+  basic_weak_simulating_transition_opening: "p \<Rightarrow>\<^sub>\<flat>\<lbrace>\<nu> a\<rbrace> P a \<Longrightarrow> p \<Rightarrow>\<^sub>\<flat>\<^sup>^\<lbrace>\<nu> a\<rbrace> P a"
 
-(** Lifted weak basic operational semantics **)
+(** Lifted basic weak simulating operational semantics **)
 
-lemma weak_sending: "a \<triangleleft> x \<Rightarrow>\<^sub>\<flat>\<^sup>^\<lbrace>a \<triangleleft> x\<rbrace> \<zero>"
-  by (simp add: weak_silent_sending weak_basic_transition_acting)
+lemma basic_weak_simulating_sending: "a \<triangleleft> x \<Rightarrow>\<^sub>\<flat>\<^sup>^\<lbrace>a \<triangleleft> x\<rbrace> \<zero>"
+  by (simp add: basic_weak_transition_sending basic_weak_simulating_transition_acting)
 
-lemma weak_receiving: "a \<triangleright> x. P x \<Rightarrow>\<^sub>\<flat>\<^sup>^\<lbrace>a \<triangleright> x\<rbrace> P x"
-  by (simp add: weak_silent_receiving weak_basic_transition_acting)
+lemma basic_weak_simulating_receiving: "a \<triangleright> x. P x \<Rightarrow>\<^sub>\<flat>\<^sup>^\<lbrace>a \<triangleright> x\<rbrace> P x"
+  by (simp add: basic_weak_transition_receiving basic_weak_simulating_transition_acting)
 
 end
