6be6fd2fc33ba4852a1af2326708c266490f54b8
Wolfgang Jeltsch <wolfgang@well-typed.com>
2019-03-26 00:57:54 +0200

    Add an implementation note

--- Isabelle/Chi_Calculus_Examples/Relaying.thy
+++ Isabelle/Chi_Calculus_Examples/Relaying.thy
@@ -15,5 +15,6 @@ lemma source_shift: "a \<leftrightarrow> b \<parallel> a \<triangleright>\<^sup>
 
 lemma dead_end_collapse: "\<nu> b. (\<currency>\<^sup>*b \<parallel> a \<leftrightarrow> b) \<approx>\<^sub>\<sharp> \<currency>\<^sup>*a"
   sorry
+  (* NOTE: The \<open>\<currency>\<^sup>?b\<close>-part of \<open>\<currency>\<^sup>*b\<close> can be handled with \<open>source_shift\<close> *)
 
 end
