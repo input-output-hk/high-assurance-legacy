65211db7cfc3811c62c5bd077b88fbcfffc2c837
Wolfgang Jeltsch <wolfgang@well-typed.com>
2019-07-06 14:37:42 +0300

    Rename goal preparation attribute
    The new name, `equivalence_simp_goal_preparation`, as opposed to the old
    one, `equivalence_goal_preparation`, make it clear that this attribute
    refers to the `equivalence_simp` proof method, not just to equivalences.

--- Isabelle/Utilities/Equivalences.thy
+++ Isabelle/Utilities/Equivalences.thy
@@ -2,10 +2,10 @@ theory Equivalences
   imports Main "HOL-Eisbach.Eisbach"
 begin
 
-named_theorems equivalence_goal_preparation and equivalence_simp
+named_theorems equivalence_simp_goal_preparation and equivalence_simp
 
 method equivalence_simp = (
-  simp only: equivalence_goal_preparation [THEN sym];
+  simp only: equivalence_simp_goal_preparation [THEN sym];
     simp only: equivalence_simp [transferred]
 )
 
