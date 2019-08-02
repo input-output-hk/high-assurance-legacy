eab95068d86b2880359c346fedd7d1409a7b820e
Javier Díaz <javier.diaz@iohk.io>
2019-08-02 15:37:01 -0300

    Move `Relaying` contents to `Communication`

--- Isabelle/Chi_Calculus_Examples/Diamond.thy
+++ Isabelle/Chi_Calculus_Examples/Diamond.thy
@@ -4,7 +4,7 @@ section \<open>Diamond-Shaped Relaying Network\<close>
 
 theory Diamond
   imports
-    Chi_Calculus.Relaying
+    Chi_Calculus.Communication
     Chi_Calculus.Proper_Weak_Transition_System
     Chi_Calculus.Basic_Weak_Transition_System
 begin
